bfdd657a59cc8a019cedfae2564544f4a32dc373
D Abhinav Vardhan <vardhanabhi22@gmail.com>
2024-04-03 21:24:25 +0530

    New

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+var firebaseConfig = {
+
+    apiKey: "AIzaSyCNBc3_wZ1zc3mia-qyMDDCCOtxxOsZuog",
+    authDomain: "collegetodo-1afb9.firebaseapp.com",
+    projectId: "collegetodo-1afb9",
+    storageBucket: "collegetodo-1afb9.appspot.com",
+    messagingSenderId: "164308723959",
+    appId: "1:164308723959:web:b27708d42369a1a1d4cbf2",
+    measurementId: "G-R5K18YC4W9"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
