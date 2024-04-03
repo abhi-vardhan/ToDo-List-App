890b5aeaf106769b08b67a05c2e807aa3806f858
D Abhinav Vardhan <vardhanabhi22@gmail.com>
2024-04-03 21:13:48 +0530

    My First commit

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ rest is your choice ^_^
 
 var firebaseConfig = {
 
-    apiKey: "<-->",
-    authDomain: "<-->",
-    databaseURL: "<-->",
-    projectId: "<-->",
-    storageBucket: "<-->",
-    messagingSenderId: "<-->",
-    appId: "<-->",
-    measurementId: "<-->"
+    apiKey: "AIzaSyCNBc3_wZ1zc3mia-qyMDDCCOtxxOsZuog",
+    authDomain: "collegetodo-1afb9.firebaseapp.com",
+    projectId: "collegetodo-1afb9",
+    storageBucket: "collegetodo-1afb9.appspot.com",
+    messagingSenderId: "164308723959",
+    appId: "1:164308723959:web:b27708d42369a1a1d4cbf2",
+    measurementId: "G-R5K18YC4W9"
 
 };
 
